05d2ca6c75d33c3313196585ba6995b5bbf3ce16
MVCionOld <tsionmichel1999@gmail.com>
2018-05-05 18:15:09 +0300

    Minor changes

--- bot.py
+++ bot.py
@@ -7,39 +7,37 @@ import config
 import urban
 
 
-def start():
-
-    debug = True
+def run_bot(debug=True):
 
     bot = telebot.TeleBot(
         config.URBAN_BOT_TOKEN if len(sys.argv) == 1 else sys.argv[-1]
     )
 
-    with open('botCommands.json') as bot_data:
-        bot_data = json.loads(bot_data.read())
+    with open('botCommands.json') as bot_activity_file:
+        bot_activity = json.loads(bot_activity_file.read())
 
     @bot.message_handler(commands=['start', 'help'])
     def handle_start_help(message):
-        bot.send_message(message.chat.id, bot_data['commands'][message.text])
+        bot.send_message(message.chat.id, bot_activity['commands'][message.text])
 
     @bot.message_handler(content_types=['text'])
     def get_explanation(message):
 
         if len(message.text.split()) > 1:
-            explanation = urban.search("+".join(message.text.split()), debug=False)
+            explanation = urban.search("+".join(message.text.split()), debug=debug)
         else:
-            explanation = urban.search(message.text, debug=False)
+            explanation = urban.search(message.text, debug=debug)
 
         if debug:
             print("[{0}, {1}]:\n{2}\n".format(message.chat.id, message.text, explanation.strip()))
 
         if explanation is None:
             explanation = "There is no word's '{}' explanation on Urban Dictionary."
-            
+
         bot.send_message(message.chat.id, explanation.strip().format(message.text))
 
     bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
-    start()
+    run_bot()
--- urban.py
+++ urban.py
@@ -1,28 +1,31 @@
 import bs4
 import requests
 
-url = "http://www.urbandictionary.com/define.php?term=%s"
+url_pattern = "http://www.urbandictionary.com/define.php?term=%s"
 
 
-def search(word):
+def search(word, debug=True):
+
     # get request of searching word
-    r = requests.get(url % word)
+    r = requests.get(url_pattern % word)
 
     # action for invalid url or wrong word
     if r.status_code != requests.codes.ok:
-        print('Wrong url or unknown word!')
-        print('Error: [%s]' % r.status_code)
+        if debug:
+            print('Wrong url or unknown word!')
+            print('Error: [%s]' % r.status_code)
         return None
 
-    # get beautiful soup of request content
     bs_obj = bs4.BeautifulSoup(r.content, 'html.parser')
-
-    # get string explanation
     exp = bs_obj.find('div', {'class': 'meaning'}).get_text()
 
-    return exp
+    return exp.strip()
 
 
 if __name__ == '__main__':
     while True:
-        pass
+        request = input()
+        if request == 'exit':
+            break
+        else:
+            print(search(request))
