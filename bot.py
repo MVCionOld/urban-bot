6fbfe286111f4abac7784c493940b82974583c41
MVCionOld <tsionmichel1999@gmail.com>
2018-05-05 16:01:23 +0300

    Work in flow

--- bot.py
+++ bot.py
@@ -1,53 +1,36 @@
-#!/usr/bin/env python3.5
-# -*- coding: utf-8 -*-
-
-
-
 import sys
 import json
 import config
 import telebot
-from urban import searchUrbanDictionary
+from urban import search
 
 
+def start():
+    debug = True
+    bot = telebot.TeleBot(
+        config.URBAN_BOT_TOKEN if len(sys.argv) == 1 else sys.argv[-1]
+    )
+    with open('botData.json') as bot_data:
+        bot_data = json.loads(bot_data.read())
 
-def main():
+    @bot.message_handler(commands=['start', 'help'])
+    def handle_start_help(message):
+        bot.send_message(message.chat.id, bot_data['commands'][message.text])
 
-	debug = True
-	bot = telebot.TeleBot(
-		config.URBAN_BOT_TOKEN if len(sys.argv) == 1 else sys.argv[-1])
-	botData = json.loads(open('botData.json').read())
+    @bot.message_handler(content_types=['text'])
+    def get_explanation(message):
+        if len(message.text.split()) > 1:
+            explanation = search("+".join(message.text.split()))
+        else:
+            explanation = search(message.text)
+        if debug:
+            print("[{0}, {1}]:\n{2}\n".format(message.chat.id, message.text, explanation.strip()))
+        if explanation is None:
+            explanation = "There is no word's '{}' explanation on Urban Dictionary."
+        bot.send_message(message.chat.id, explanation.strip().format(message.text))
 
-
-	@bot.message_handler(commands=['start', 'help'])
-	def handle_start_help(message):
-		bot.send_message(message.chat.id, botData['commands'][message.text])
-		
-	@bot.message_handler(content_types=['text'])
-	def get_explanation(message):
-		explanation = None
-		if len(message.text.split()) > 1:
-			explanation = searchUrbanDictionary("+".join(message.text.split()))
-		else:
-			explanation = searchUrbanDictionary(message.text)
-		if debug: 
-			print("[{0}, {1}]:\n{2}\n".format(message.chat.id, message.text, explanation.strip()))
-		if explanation is None:
-			explanation = "There is no word's '{}' explanation on Urban Dictionary."
-		bot.send_message(message.chat.id, explanation.strip().format(message.text))
-
-
-	bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
 
 
-	
 if __name__ == '__main__':
-	main()
-	while True:
-		try:
-			main()
-		except:
-			print('Reloading...')
-			pass
-		import time
-		time.sleep(5)
+    start()
--- urban.py
+++ urban.py
@@ -1,27 +1,15 @@
-#!/usr/bin/env python3.5
-
-
 import bs4
 import requests
-import sys, os, glob
-import re
 
 
-def searchUrbanDictionary(word):
-    
-    """
-    Function for search explanation of urban terms.
-    Example:
-        >>> exp = searchUrbanDictionary('asshole')
-		>>> exp
-		Your current boss.
-    """
+url = "http://www.urbandictionary.com/define.php?term=%s"
 
-    url = "http://www.urbandictionary.com/define.php?term=%s"
+
+def search(word):
 
     # get request of searching word 
     r = requests.get(url % word)
-    
+
     # action for invalid url or wrong word
     if r.status_code != requests.codes.ok:
         print('Wrong url or unknown word!')
@@ -29,19 +17,9 @@ def searchUrbanDictionary(word):
         return None
 
     # get beautiful soup of request content
-    bsObj = bs4.BeautifulSoup(r.content, 'html.parser')
+    bs_obj = bs4.BeautifulSoup(r.content, 'html.parser')
 
     # get string explanation
-    exp = bsObj.find('div', {'class' : 'meaning'}).get_text()
+    exp = bs_obj.find('div', {'class': 'meaning'}).get_text()
 
     return exp
-	
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Attention: no words for search.')
-        sys.exit(1)
-    for word in sys.argv[1:]:
-        print(('-'*10 + ' %s ' + '-'*10) % word)
-        print(searchUrbanDictionary(word).strip(), end = '\n\n')
-    sys.exit(0)
