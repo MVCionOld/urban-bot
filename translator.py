2923c6b8d20148b3080458f6c4d418c1d1c4b3df
MVCionOld <tsionmichel1999@gmail.com>
2018-05-09 23:04:31 +0300

    Work in flow

--- translator.py
+++ translator.py
@@ -1,7 +1,8 @@
 import yandex_translate
 
-translator = yandex_translate.YandexTranslate(
-    "trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+import config
+
+translator = yandex_translate.YandexTranslate(config.YANDEX_TRANSLATE_API)
 
 #print('Languages:', translate.langs)
 #print('Translate directions:', translate.directions)
