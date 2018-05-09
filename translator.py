880ca542b08f918d8fa0188a8f578df6d18be281
MVCionOld <tsionmichel1999@gmail.com>
2018-05-09 22:41:27 +0300

    Work in flow

--- translator.py
+++ translator.py
@@ -1,6 +1,7 @@
 import yandex_translate
 
-translate = yandex_translate.YandexTranslate('Your API key here.')
+translate = yandex_translate.YandexTranslate(
+    "trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
 print('Languages:', translate.langs)
 print('Translate directions:', translate.directions)
 print('Detect language:', translate.detect('Привет, мир!'))
