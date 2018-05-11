24aa47d92bf6c8891f12d80500ed6a8c6ea2eb2d
MVCionOld <tsionmichel1999@gmail.com>
2018-05-12 01:27:10 +0300

    Work in flow

--- db_manager.py
+++ db_manager.py
@@ -2,99 +2,110 @@ import sqlite3
 
 import config
 
-connection = sqlite3.Connection(config.DB_NAME)
-cursor = connection.cursor()
-
 
 def add_id(chat_id):
-    query = """
-        INSERT INTO CHAT_LANG 
-          (chat_id)
-        VALUES
-          ({0});
-    """.format(chat_id)
-    cursor.execute(query)
-    connection.commit()
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            INSERT INTO CHAT_LANG 
+              (chat_id)
+            VALUES
+              ({0});
+        """.format(chat_id)
+        cursor.execute(query)
+        connection.commit()
 
 
 def get_lang(chat_id):
-    query = """
-        SELECT 
-          lang_txt
-        FROM 
-          CHAT_LANG
-        WHERE
-          chat_id = {0};
-    """.format(chat_id)
-    response = cursor.execute(query).fetchone()
-    if response is None:
-        add_id(chat_id)
-        return get_lang(chat_id)
-    else:
-        return response[0]
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            SELECT 
+              lang_txt
+            FROM 
+              CHAT_LANG
+            WHERE
+              chat_id = {0};
+        """.format(chat_id)
+        response = cursor.execute(query).fetchone()
+        if response is None:
+            add_id(chat_id)
+            return get_lang(chat_id)
+        else:
+            return response[0]
 
 
 def set_lang(chat_id, lang_txt):
-    query = """
-        UPDATE
-          CHAT_LANG
-        SET
-          lang_txt = '{1}'
-        WHERE
-          chat_id = {0};
-    """.format(chat_id, lang_txt,)
-    q = cursor.execute(query)
-    connection.commit()
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            UPDATE
+              CHAT_LANG
+            SET
+              lang_txt = '{1}'
+            WHERE
+              chat_id = {0};
+        """.format(chat_id, lang_txt,)
+        cursor.execute(query)
+        connection.commit()
 
 
 def add_description(request_txt, description_txt):
-    cursor.execute("""
-        INSERT INTO REQUEST 
-          (urban_request_txt, description_txt)
-        VALUES
-          ('{0}', '{1}'); 
-    """.format(request_txt.lower(), description_txt))
-    connection.commit()
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO REQUEST 
+              (urban_request_txt, description_txt)
+            VALUES
+              ('{0}', '{1}'); 
+        """.format(request_txt.lower(), description_txt))
+        connection.commit()
 
 
 def update_cnt(request_txt):
-    query = """
-        UPDATE 
-          REQUEST
-        SET
-          frequency_cnt = frequency_cnt + 1
-        WHERE
-          urban_request_txt = '{0}';
-    """.format(request_txt.lower(),)
-    cursor.execute(query)
-    connection.commit()
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            UPDATE 
+              REQUEST
+            SET
+              frequency_cnt = frequency_cnt + 1
+            WHERE
+              urban_request_txt = '{0}';
+        """.format(request_txt.lower(),)
+        cursor.execute(query)
+        connection.commit()
 
 
 def get_description(request_txt):
-    query = """
-        SELECT 
-          description_txt
-        FROM 
-          REQUEST
-        WHERE
-          urban_request_txt = '{0}';
-    """.format(request_txt.lower())
-    response = cursor.execute(query).fetchone()
-    if response is None:
-        return None
-    else:
-        update_cnt(request_txt)
-        return response[0]
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            SELECT 
+              description_txt
+            FROM 
+              REQUEST
+            WHERE
+              urban_request_txt = '{0}';
+        """.format(request_txt.lower())
+        response = cursor.execute(query).fetchone()
+        if response is None:
+            return None
+        else:
+            update_cnt(request_txt)
+            return response[0]
 
 
 def get_top(limit):
-    query = """
-        SELECT 
-          urban_request_txt
-        FROM 
-          REQUEST
-        ORDER BY
-          frequency_cnt DESC
-        LIMIT {0};
-    """.format(limit,)
-    return cursor.execute(query).fetchall()
+    with sqlite3.Connection(config.DB_NAME) as connection:
+        cursor = connection.cursor()
+        query = """
+            SELECT 
+              urban_request_txt
+            FROM 
+              REQUEST
+            ORDER BY
+              frequency_cnt DESC
+            LIMIT {0};
+        """.format(limit,)
+        return cursor.execute(query).fetchall()
