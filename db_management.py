3821f6773ed91a84d398fc8cf81573e5ee153bd4
MVCionOld <tsionmichel1999@gmail.com>
2018-05-12 00:37:19 +0300

    Work in flow

--- db_management.py
+++ db_management.py
@@ -20,14 +20,18 @@ def add_id(chat_id):
 def get_lang(chat_id):
     query = """
         SELECT 
-          CHAT_LANG.lang_txt
+          lang_txt
         FROM 
           CHAT_LANG
         WHERE
-          CHAT_LANG.chat_id = {0};
+          chat_id = {0};
     """.format(chat_id)
-    response = cursor.execute(query)
-    return response
+    response = cursor.execute(query).fetchone()
+    if response is None:
+        add_id(chat_id)
+        return get_lang(chat_id)
+    else:
+        return response[0]
 
 
 def set_lang(chat_id, lang_txt):
@@ -35,11 +39,11 @@ def set_lang(chat_id, lang_txt):
         UPDATE
           CHAT_LANG
         SET
-          CHAT_LANG.lang_txt = {1}
+          lang_txt = '{1}'
         WHERE
-          CHAT_LANG.chat_id = {0};
-    """.format(chat_id, lang_txt)
-    cursor.execute(query)
+          chat_id = {0};
+    """.format(chat_id, lang_txt,)
+    q = cursor.execute(query)
     connection.commit()
 
 
@@ -48,7 +52,7 @@ def add_description(request_txt, description_txt):
         INSERT INTO REQUEST 
           (urban_request_txt, description_txt)
         VALUES
-          ({0}, {1}); 
+          ('{0}', '{1}'); 
     """.format(request_txt.lower(), description_txt))
     connection.commit()
 
@@ -56,14 +60,28 @@ def add_description(request_txt, description_txt):
 def get_description(request_txt):
     query = """
         SELECT 
-          REQUEST.description_txt
+          description_txt
         FROM 
           REQUEST
         WHERE
-          REQUEST.urban_request_txt = {0};
+          urban_request_txt = '{0}';
     """.format(request_txt.lower())
     response = cursor.execute(query)
     return response
 
 
+def get_top(limit):
+    query = """
+        SELECT 
+          REQUEST.urban_request_txt
+        FROM 
+          REQUEST
+        ORDER BY
+          REQUEST.frequency_cnt DESC
+        LIMIT {0};
+    """.format(limit,)
+    return cursor.execute(query).fetchall()
 
+
+if __name__ == '__main__':
+    print(get_lang(1032567))
