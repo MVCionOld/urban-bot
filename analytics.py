9ac94b47088a1e554f5bda5c0fb3e777dacba7e8
MVCionOld <tsionmichel1999@gmail.com>
2018-05-13 00:20:41 +0300

    Analytics is in developing

--- analytics.py
+++ analytics.py
@@ -43,27 +43,26 @@ def language_frequency(chat_id):
         matplotlib.pyplot.xlabel("%", fontsize=extended_cnt * 4)
         matplotlib.pyplot.ylabel("", fontsize=extended_cnt * 4)
         matplotlib.pyplot.title("", fontsize=extended_cnt * 5)
-        pic_name = 'tmp/{}.png'.format(str(datetime.datetime.now()) + str(chat_id))
+        pic_name = 'tmp/lang_{}.png'.format(str(datetime.datetime.now()) + str(chat_id))
         matplotlib.pyplot.savefig(pic_name)
         return pic_name
 
 
 def request_frequency(chat_id):
-    with sqlite3.connect(config.DB_NAME) as connection:
-        requests, frequences = [], []
-        for _, request, cnt in db_manager.get_top(10):
-            requests.append(request)
-            frequences.append(cnt)
-        extended_cnt = len(requests)
-        pos = numpy.arange(extended_cnt)
-        matplotlib.pyplot.barh(pos, frequences, color='blue', edgecolor='black')
-        matplotlib.pyplot.yticks(pos, requests)
-        matplotlib.pyplot.xlabel("", fontsize=extended_cnt * 4)
-        matplotlib.pyplot.ylabel("", fontsize=extended_cnt * 4)
-        matplotlib.pyplot.title("", fontsize=extended_cnt * 5)
-        pic_name = 'tmp/{}.png'.format(str(datetime.datetime.now()) + str(chat_id))
-        matplotlib.pyplot.savefig(pic_name)
-        return pic_name
+    requests, frequences = [], []
+    for _, request, cnt in db_manager.get_top(10):
+        requests.append(request)
+        frequences.append(cnt)
+    extended_cnt = len(requests)
+    pos = numpy.arange(extended_cnt)
+    matplotlib.pyplot.barh(pos, frequences, color='blue', edgecolor='black')
+    matplotlib.pyplot.yticks(pos, requests)
+    matplotlib.pyplot.xlabel("", fontsize=extended_cnt)
+    matplotlib.pyplot.ylabel("", fontsize=extended_cnt)
+    matplotlib.pyplot.title("", fontsize=extended_cnt)
+    pic_name = 'tmp/req_{}.png'.format(str(datetime.datetime.now()) + str(chat_id))
+    matplotlib.pyplot.savefig(pic_name)
+    return pic_name
 
 
 request_frequency(0)
